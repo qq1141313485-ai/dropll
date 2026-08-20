4df24174810699ec6264ebf1dbeacd6357bb34c0
qq1141313485-ai <301659129+qq1141313485-ai@users.noreply.github.com>
2026-08-20 19:04:06 +0800

    build: use official Android dependency repositories

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -11,9 +11,6 @@ pluginManagement {
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/public")
-        maven("https://maven.aliyun.com/repository/gradle-plugin")
         google()
         mavenCentral()
         gradlePluginPortal()
